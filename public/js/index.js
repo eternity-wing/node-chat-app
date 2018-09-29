3b2c007c91cc3688f60e9fda89794906cc6f5098
wing <eternity-wing>
2018-09-29 21:08:48 +0330

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on('connect', function(){
     console.log('Connected to server');
-    socket.emit('createMessage', {
-        from:"mosyClient",
-        text:"jasem"
-    });
 });
 
 socket.on('disconnect', function(data){
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 
-    socket.emit('newMessage', {
-        from: "mosy@gmail.com",
-        text: "salam",
-        createAt: new Date()
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage ', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
