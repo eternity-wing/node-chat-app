3bd07493ba90abd34e1d56cc189771098daacf3e
wing <eternity-wing>
2018-10-05 10:42:52 +0330

    Add Users class and updateUserlIST EVENT

---
+++ public/js/chat.js
@@ -0,0 +1,108 @@
+var socket = io();
+
+function scrollToBottom(){
+    //selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
+
+socket.on('connect', function(){
+    console.log('Connected to server');
+    var params = jQuery.deparam(window.location.search);
+
+    socket.emit('join', params, function(err){
+        if(err){
+            alert(err);
+            window.location.href = '/';
+        }else{
+            console.log('No error');
+        }
+    });
+});
+
+socket.on('disconnect', function(data){
+    console.log('Disconnect from server');
+});
+
+socket.on('updateUserList', function(users){
+    var ol = jQuery('<ol></ol>');
+    users.forEach(function(user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+    jQuery("#users").html(ol);
+});
+
+socket.on('newMessage', function(message){
+    var formatedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery("#message-template").html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formatedTime
+    });
+
+    jQuery("#messages").append(html);
+    scrollToBottom();
+
+});
+
+socket.on('newLocationMessage', function(message){
+    console.log('newLocationMessage ', message);
+    var formatedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery("#location-message-template").html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        createdAt: formatedTime,
+        url: message.url
+    });
+    jQuery("#messages").append(html);
+    scrollToBottom();
+});
+
+
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+    var messageTextBox = jQuery('[name=message]');
+    socket.emit('createMessage', {
+        from: 'User',
+        text: messageTextBox.val()
+    }, function(data){
+        messageTextBox.val('');
+    });
+
+});
+
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click', function(){
+    if(!navigator.geolocation){
+        return alert('Geolocation not supported by your browser');
+    }
+    locationButton.attr('disabled', 'disabled').text('Sending location ...');
+
+    navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send location');
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+        });
+    }, function(){
+        locationButton.removeAttr('disabled').text('Send location');
+        alert('Unable to fetch location.');
+    });
+
+
+
+});
